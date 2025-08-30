097f5b85a0e972deaca48ea7b60b1641c7f04741
jaysheng2010 <ooijaysheng@gmail.com>
2025-08-30 12:17:25 +0800

    Update receive_order.py

--- receive_order.py
+++ receive_order.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, redirect, url_for, request,session
 from flask_cors import CORS
 from flask_limiter import Limiter
-from flask_limiter_util import get_remote_address
+from flask_limiter.util import get_remote_address
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
